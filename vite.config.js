148ff4dcc9021d36cd08b995d877f678acfe18f3
Serhii Hodis <serhodg12@gmail.com>
2025-11-18 10:47:21 +0200

    Config updated

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  base: "tailwind-vite",
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+      }
+    }
+  }
 })
